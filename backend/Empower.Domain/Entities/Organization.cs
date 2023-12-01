519a1a4f4527037d14b28886c0f70f96d15a8951
luca0898 <lucaguiraldello@hotmail.com>
2023-12-01 10:45:41 -0300

    Adicionando algumas entidades

--- backend/Empower.Domain/Entities/Organization.cs
+++ backend/Empower.Domain/Entities/Organization.cs
@@ -6,5 +6,11 @@ namespace Empower.Domain.Entities
     {
         public string? Name { get; set; }
         public string? ImageUrl { get; set; }
+
+        public Guid? AdminId { get; set; }
+        public User? Admin { get; set; }
+
+        public IEnumerable<User>? Managers { get; set; }
+        public IEnumerable<User>? Users { get; set; }
     }
 }
---
+++ backend/Empower.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Empower.Domain.Entities.Shared;
+
+namespace Empower.Domain.Entities;
+
+public class User : Entity<Guid>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName { get { return FirstName + LastName; } }
+    public string Email { get; set; }
+    public string JobTitle { get; set; }
+    public string AvatarUrl { get; set; }
+    public string PasswordHash { get; set; }
+}
